287ef09b0553cf287211f8675edae87a948a07b1
Youlun <127579610+youlun@users.noreply.github.com>
2025-05-06 11:09:09 -0400

    just a save point

---
+++ script.js
@@ -0,0 +1,10 @@
+const container = document.querySelector("#container");
+
+const content = document.createElement("div");
+content.classList.add("content");
+content.textContent = "This is the glorious text-content!";
+
+container.appendChild(content);
+
+
+const p = document.createElement("p");
